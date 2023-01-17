40b6d5725b09b608103a4ce05ec9952de9dde771
Kadigas <adbagas05@gmail.com>
2023-01-17 20:05:03 +0700

    feat: added struct, modified db migrations

--- database/sql_migrations/1_initiate.sql
+++ database/sql_migrations/1_initiate.sql
@@ -23,26 +23,31 @@ CREATE TABLE movie (
 	aired VARCHAR(256),
     total_episode INT,
     status VARCHAR(100),
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW(),
     category_id BIGINT,
-    type_id BIGINT
-);
-
-CREATE TABLE account (
-    id SERIAL NOT NULL PRIMARY KEY,
-    username VARCHAR(256),
-    password VARCHAR(256),
-    name VARCHAR(256),
-    email VARCHAR(256),
-    image_url VARCHAR(256),
+    type_id BIGINT,
     created_at TIMESTAMP DEFAULT NOW(),
     updated_at TIMESTAMP DEFAULT NOW()
 );
 
-CREATE TABLE watchlist(
+CREATE TABLE account (
     id SERIAL NOT NULL PRIMARY KEY,
+    email VARCHAR(256),
+    password VARCHAR(256),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE users (
+    id SERIAL NOT NULL PRIMARY KEY,
+    username VARCHAR(256),
+    image_url VARCHAR(256),
     account_id BIGINT,
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE watchlist (
+    id SERIAL NOT NULL PRIMARY KEY,
+    users_id BIGINT,
     movie_id BIGINT
 );
 
@@ -52,8 +57,11 @@ ADD CONSTRAINT fk_movie_category FOREIGN KEY (category_id) REFERENCES category (
 ALTER TABLE movie
 ADD CONSTRAINT fk_movie_type FOREIGN KEY (type_id) REFERENCES type (id);
 
+ALTER TABLE users
+ADD CONSTRAINT fk_users_account FOREIGN KEY (account_id) REFERENCES account (id);
+
 ALTER TABLE watchlist
-ADD CONSTRAINT fk_watchlist_account FOREIGN KEY (account_id) REFERENCES account (id);
+ADD CONSTRAINT fk_watchlist_users FOREIGN KEY (users_id) REFERENCES users (id);
 
 ALTER TABLE watchlist
 ADD CONSTRAINT fk_watchlist_movie FOREIGN KEY (movie_id) REFERENCES movie (id);
---
+++ structs/structs.go
@@ -0,0 +1,51 @@
+package structs
+
+type Category struct {
+	ID         int64  `json:"id"`
+	Name       string `json:"name"`
+	Created_at string `json:"created_at"`
+	Updated_at string `json:"updated_at"`
+}
+
+type Type struct {
+	ID         int64  `json:"id"`
+	Name       string `json:"name"`
+	Created_at string `json:"created_at"`
+	Updated_at string `json:"updated_at"`
+}
+
+type Movie struct {
+	ID            int64  `json:"id"`
+	Title         string `json:"title"`
+	Description   string `json:"description"`
+	Image_url     string `json:"image_url"`
+	Aired         string `json:"aired"`
+	Total_episode int    `json:"total_episode"`
+	Status        string `json:"status"`
+	Category_id   int64  `json:"category_id"`
+	Type_id       int64  `json:"type_id"`
+	Created_at    string `json:"created_at"`
+	Updated_at    string `json:"updated_at"`
+}
+
+type Account struct {
+	ID         int64  `json:"id"`
+	Email      string `json:"email"`
+	Password   string `json:"password"`
+	Created_at string `json:"created_at"`
+	Updated_at string `json:"updated_at"`
+}
+
+type Users struct {
+	ID         int64  `json:"id"`
+	Username   string `json:"username"`
+	Image_url  string `json:"image_url"`
+	Account_id int64  `json:"account_id"`
+	Updated_at string `json:"updated_at"`
+}
+
+type Watchlist struct {
+	ID       int64 `json:"id"`
+	Users_id int64 `json:"users_id"`
+	Movie_id int64 `json:"movie_id"`
+}
