09504e18d1f39cb116a6de17eb7ca5869bd58b8b
Kadigas <adbagas05@gmail.com>
2023-01-18 15:38:22 +0700

    feat: modified json for air_date

--- structs/structs.go
+++ structs/structs.go
@@ -19,7 +19,7 @@ type Movie struct {
 	Title          string `json:"title"`
 	Description    string `json:"description"`
 	Image_url      string `json:"image_url"`
-	Air_date       string `json:"aired"`
+	Air_date       string `json:"air_date"`
 	Total_episode  string `json:"total_episode"`
 	Current_status string `json:"current_status"`
 	Category_id    int64  `json:"category_id"`
